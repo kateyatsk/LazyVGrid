8af5aeaeabc8d06931894c9db2ba80663b88e664
kateyatsk <klaettoe2023@gmail.com>
2025-05-09 00:45:48 +0300

    Initial Commit

---
+++ LazyVGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyVGrid
+//
+//  Created by Екатерина Яцкевич on 9.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyVGrid/LazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridApp.swift
+//  LazyVGrid
+//
+//  Created by Екатерина Яцкевич on 9.05.25.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
